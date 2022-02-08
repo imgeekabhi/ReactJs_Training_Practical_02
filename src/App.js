c6fdbf1551c8f4b7146cad6939cff5e617064f88
Abhishek <imgeekabhi@gmail.com>
2022-02-08 19:06:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+function App(params) {
+  return (
+    <>
+      <h1>Hello Everyone!</h1>
+      <p>
+        I made this React App without using <b>create-react-app</b> command.
+      </p>
+      <p></p>
+    </>
+  );
+}
+export default App;
